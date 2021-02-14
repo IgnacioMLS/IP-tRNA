319233bbb697b8399214983c4363b7e1496632d6
IgnacioMLS <78439288+IgnacioMLS@users.noreply.github.com>
2021-02-14 19:03:03 +0100

    Add files via upload

---
+++ Scripts/Aln_M1G.py
@@ -0,0 +1,61 @@
+'''
+This script performs the alignment against the mature genome allowing 1 mismatch in the seed.
+'''
+
+# Built-in/Generic Imports 
+import os 
+import sys
+import subprocess
+import multiprocessing
+
+nucleos = multiprocessing.cpu_count()
+
+# Arguments
+fullCmdArguments= sys.argv
+argumentList= fullCmdArguments[1:]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+listdir=os.listdir(dir_path)
+dir_path.split('/')
+path=dir_path.split('/')
+path=path[:-1]
+refgen_path=('/').join(path)+'/Reference_Genomes/'
+sample=sys.argv[1]
+sample_name=sample.split('.')[0]
+func=os.path.abspath(os.getcwd())
+
+
+if os.path.exists('../Results/'+sample_name):
+	if not os.path.exists('../Results/'+sample_name+'/Alignment_M1G'):
+			os.system('mkdir'+' '+'../Results/'+sample_name+'/Alignment_M1G')
+	else:
+		print ('There is a Results folder for the mature genome alignment already created for this sample ! Maybe the analysis has already been done (Cheek the Results folder)')
+else:
+	print ('The results folder for this sample has not been created so the first step has not been done ! ')
+	raise SystemExit
+
+os.chdir('../Results/'+sample_name+'/Alignment_M1G/')
+
+
+# Alignment
+print ('4-Performing the alignment against the mature genome (1 mismatch in the seed)')
+command = "bowtie2 --local -p " + str(nucleos)
+os.system(command+ ' -N 1 -x '+refgen_path+'families_tRNA_refgenome '+'../Alignment_PG/'+sample_name+'_unmapped_PGloc.fastq'+' --un '+sample_name+'_unmapped_MG_1M_loc.fastq'+' | samtools view -bSF4 - > '+sample_name+'_MG_1M_loc_mapped.bam')
+
+# Processing files (sorting and indexing)
+os.system('samtools sort '+sample_name+'_MG_1M_loc_mapped.bam'+ ' -o ' +sample_name+'_MG_1M_loc_mapped_sort.bam')
+os.system('samtools index '+sample_name+'_MG_1M_loc_mapped_sort.bam')   
+
+# Removing soft clipped bases
+os.system('python '+func+'/rm_soft_clipped_bases.py '+sample_name+' '+'_MG_1M_loc_mapped_sort')
+
+# Processing files (sorting and indexing)
+os.system('samtools sort '+sample_name+'_MG_1M_loc_mapped_sort_soft_clipped_rm.bam'+ ' -o ' +sample_name+'_MG_1M_loc_mapped_sort_soft_clipped_rm_sort.bam')
+os.system('samtools index '+sample_name+'_MG_1M_loc_mapped_sort_soft_clipped_rm_sort.bam') 
+
+# Marge the files for mature tRNA
+os.system('samtools merge '+'../Final_results/'+sample_name+'all_mature.bam ../Alignment_M1G/'+sample_name+'_MG_1M_loc_mapped_sort_soft_clipped_rm_sort.bam '+'../Alignment_MG/'+sample_name+'_MGloc_mapped_sort_soft_clipped_rm_sort.bam')
+
+# Processing files (sorting and indexing)
+os.system('samtools sort ../Final_results/'+sample_name+'all_mature.bam '+ ' -o ' +'../Final_results/'+sample_name+'all_mature_sort.bam')
+os.system('samtools index '+'../Final_results/'+sample_name+'all_mature_sort.bam')  
+
---
+++ Scripts/Aln_WG.py
@@ -0,0 +1,141 @@
+'''
+This script performs the alignment against the precursor genome.
+'''
+# Built-in/Generic Imports and Modules
+import argparse
+import subprocess
+import os 
+import sys
+import glob
+import re
+import multiprocessing
+import pysam
+
+nucleos = multiprocessing.cpu_count()
+
+# Sample name
+sample=sys.argv[1]
+folder=sys.argv[2]
+
+print ('ANALYZING SAMPLE:',sample)
+
+# Samples folder.
+samples_path=folder
+
+#Folder with the reference genome:
+refgen_path='../Reference_Genomes/'
+func=os.path.abspath(os.getcwd())
+
+
+
+#Name of all the samples in the folder:
+samples=os.listdir(samples_path)
+
+def SAM_iterator(sam_filename):
+    '''Parse function for Sam file'''
+    samfile = pysam.AlignmentFile(sam_filename, "rb")
+    for line in samfile:
+        list_read=str(line)
+        read=(list_read.split('\t'))
+        yield (read)
+
+
+if sample in samples:
+    sample_1=sample.split('.')
+    sample_name=sample_1[0]
+
+    if sample_1[1]=='fa' or sample_1[1]=='fastq':
+        #####ALIGNMENT WITH THE WHOLE GENOME#######
+	
+        if not os.path.exists('../Results/'+sample_name+'/Alignment_WG/'):
+            os.makedirs('../Results/'+sample_name+'/Alignment_WG/')
+        
+        print ('1-Performing the Alignment against the whole genome:')
+        command = "bowtie2 --local -p "+ str(nucleos)
+        
+        #os.system(command + ' -N 0 -x '+refgen_path+'genome'+' '+samples_path + "/"+sample+' --un ../Results/'+sample_name+'/Alignment_WG/'+sample_name+'_unmapped_WGloc.fastq'+' | samtools view -bSF4 - > '+'../Results/'+sample_name+'/Alignment_WG/'+sample_name+'_WGloc_mapped.bam')
+        
+        #Set working directory.
+        os.chdir('../Results/'+sample_name+'/Alignment_WG/')
+
+        #Processing files (sorting and indexing).
+        os.system('samtools sort '+sample_name+'_WGloc_mapped.bam'+ ' -o ' +sample_name+'_WGloc_mapped_sort.bam')
+        os.system('samtools index '+sample_name+'_WGloc_mapped_sort.bam')   
+		
+
+
+        print ('1.2-Filtering reads. Obtain reads that are mapping to tRNAs')
+		
+        #Obtain the reads that correspond to tRNA only.
+        os.system('samtools view -b -L '+'../../../Reference_Genomes/info/tRNA_only.bed'+' '+sample_name+'_WGloc_mapped.bam > '+sample_name+'_WGloc_only_trna.bam')
+		
+        #Processing files (sorting and indexing).
+        os.system('samtools sort '+sample_name+'_WGloc_only_trna.bam'+ ' -o ' +sample_name+'_WGloc_only_trna_sort.bam')
+        os.system('samtools index '+sample_name+'_WGloc_only_trna_sort.bam') 
+
+        
+        #Removing soft clipped bases
+        os.system('python '+func+'/rm_soft_clipped_bases.py '+sample_name+' '+'_WGloc_only_trna_sort')
+
+        #Processing files (sorting and indexing).
+        os.system('samtools sort '+sample_name+'_WGloc_only_trna_sort_soft_clipped_rm.bam'+ ' -o ' +sample_name+'_WGloc_only_trna_soft_clipped_rm_sort.bam')
+        os.system('samtools index '+sample_name+'_WGloc_only_trna_soft_clipped_rm_sort.bam') 
+
+
+
+        print ('1.3-Filtering and calssification of the reads. Classifing reads between precursor and mature sequencies')
+		
+        #Extract the reads that correspond to tRNA precursor only. lead_trail.bed contains the trailing regions, if a read falls in to these regions is considered a precursor. 
+        os.system('samtools view -b -L '+'../../../Reference_Genomes/info/lead_trail_ok.bed '+sample_name+'_WGloc_only_trna_soft_clipped_rm_sort.bam > '+sample_name+'_WGloc_only_trna_precursor.bam')
+		
+		#Sort the bam file.
+        os.system('samtools sort '+sample_name+'_WGloc_only_trna_precursor.bam'+ ' -o ' +sample_name+'_WGloc_only_trna_precursor_sort.bam')
+		
+		#Index the bam file.
+        os.system('samtools index '+sample_name+'_WGloc_only_trna_precursor_sort.bam')   
+		
+		#Obtain the reads in sam format.
+        os.system('samtools view -h -o '+sample_name+'_WGloc_only_trna_precursor_sort.sam '+sample_name+'_WGloc_only_trna_precursor_sort.bam')
+		
+		#Filter the precursor file to detect reads that have a deletion so they don't have the intronic region and they should be mature reads. 	
+        out_id= open(sample_name+'intron_delet_mature.txt',"w")
+        count_mapq=0
+        all_reads=0
+        fd = open(sample_name+'_WGloc_only_trna_precursor_sort.sam',"r")
+        for aln in SAM_iterator(fd):
+            qname=str(aln[0])
+            cigar=str(aln[5])
+            seq=str(aln[9])
+            if 'D' in cigar:
+                for num1, i_or_d, num2, m in re.findall('(\d+)([ID])(\d+)?([A-Za-z])?', cigar):
+                    if int(num1) > 3:
+                        out_id.write(qname+'\n')
+        out_id.close()
+        
+		#change comand line !!!! https://github.com/broadinstitute/picard/wiki/Command-Line-Syntax-Transition-For-Users-(Pre-Transition)
+        os.system('picard FilterSamReads '+'I='+sample_name+'_WGloc_only_trna_precursor_sort.bam '+'O='+sample_name+'_WGloc_only_trna_with_intron_precursor.bam READ_LIST_FILE='+sample_name+'intron_delet_mature.txt FILTER=excludeReadList >/dev/null 2>&1')
+		
+		#Sort the bam file.
+        os.system('samtools sort '+sample_name+'_WGloc_only_trna_with_intron_precursor.bam'+ ' -o ' +sample_name+'_WGloc_only_trna_with_intron_precursor_sort.bam')
+
+		#Index the bam file.
+        os.system('samtools index '+sample_name+'_WGloc_only_trna_with_intron_precursor_sort.bam') 
+		
+		#Extract the reads id of the precursor 
+        os.system('samtools view -F 4 '+sample_name+'_WGloc_only_trna_with_intron_precursor_sort.bam '+'| cut -f1 | sort -u > '+sample_name+'_WGloc_only_trna_precursor_filtered.txt')
+
+		#Extact the mature reads:
+        os.system('picard FilterSamReads '+'I='+sample_name+'_WGloc_only_trna_soft_clipped_rm_sort.bam '+'O='+sample_name+'_WGloc_only_trna_mature.bam READ_LIST_FILE='+sample_name+'_WGloc_only_trna_precursor_filtered.txt FILTER=excludeReadList >/dev/null 2>&1')
+		
+		#Sort the bam file.
+        os.system('samtools sort '+sample_name+'_WGloc_only_trna_mature.bam'+ ' -o ' +sample_name+'_WGloc_only_trna_mature_sort.bam')
+		
+		#Index the bam file of tRNA precursor data only:
+        os.system('samtools index '+sample_name+'_WGloc_only_trna_mature_sort.bam')
+		
+		#Marge the mature reads +  unmmaped reads, in order to peform the aligment with the mature genome:
+        os.system('bedtools bamtofastq -i '+sample_name+'_WGloc_only_trna_mature.bam -fq '+sample_name+'_WGloc_only_trna_mature.fastq')
+
+		#Marge fastq 
+        os.system('cat '+sample_name+'_WGloc_only_trna_mature.fastq '+sample_name+'_unmapped_WGloc.fastq > '+sample_name+'_WGloc_only_trna_mature_and_unmapped.fastq')
+
